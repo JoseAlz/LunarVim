79460a3acb3f6c8a4f2c4f4412f6055471061452
kylo252 <59826753+kylo252@users.noreply.github.com>
2021-11-24 18:35:15 +0100

    fix: restore lvim-version

--- lua/lvim/plugins.lua
+++ lua/lvim/plugins.lua
@@ -270,7 +270,7 @@ return {
     config = function()
       require("lvim.core.alpha").setup()
     end,
-    -- disable = not lvim.builtin.alpha.active,
+    disable = not lvim.builtin.alpha.active,
   },
 
   -- Terminal
